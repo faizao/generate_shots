535f615974e0b228e1573b2871e1ee775a723587
Ubuntu <ubuntu@ip-172-31-32-172.ec2.internal>
2020-01-27 18:32:42 +0000

    insert file of shot and .sh

---
+++ run_batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python3 main_shot.py 1 100 &
+python3 main_shot.py 101 200 & 
+python3 main_shot.py 201 300 & 
+python3 main_shot.py 301 400 &
+python3 main_shot.py 401 500 & 
+python3 main_shot.py 501 600 & 
+python3 main_shot.py 601 700 &
+python3 main_shot.py 701 800 & 
+python3 main_shot.py 801 900 &
+python3 main_shot.py 901 1001 & 
+
+
