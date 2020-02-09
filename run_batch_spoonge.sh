22d0dfe7d24fec93fe4b20aedd879c17df43167f
jmtargino <jonas.mendonca@usp.br>
2020-02-09 01:04:31 -0300

    changed in run_batch_spoonge

---
+++ run_batch_spoonge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python3 main_shot_spoonge.py 1 200 &
+python3 main_shot_spoonge.py 201 400 & 
+python3 main_shot_spoonge.py 401 600 & 
+python3 main_shot_spoonge.py 601 800 &
+python3 main_shot_spoonge.py 801 1000 & 
+python3 main_shot_spoonge.py 1001 1200 & 
+python3 main_shot_spoonge.py 1201 1400 &
+python3 main_shot_spoonge.py 1401 1600 & 
+python3 main_shot_spoonge.py 1601 1800 &
+python3 main_shot_spoonge.py 1801 2001 & 
+
+
